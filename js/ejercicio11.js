814486f66553084bb6a61779067aa69400b5e875
marvi2011 <marvi661@hotmail.com>
2024-07-11 11:38:44 -0300

    no pude hacer los puntos 9 y 11 del tp

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,1 +1,20 @@
-const numero1 = parseInt(prompt("Ingrese un número: "))
+const numero1 = parseInt(prompt("Ingrese un número: "));
+if (numero1 % 2 === 0) {
+  if (numero1 % 3 === 0) {
+    if (numero1 % 5 === 0) {
+      if (numero1 % 7 === 0) {
+        document.write(`${numero1} es divisible entre 2, 3, 5 y 7.`);
+      } else {
+        document.write(
+          `${numero1} es divisible entre 2, 3 y 5 pero no entre 7.`
+        );
+      }
+    } else {
+      document.write(`${numero1} es divisible entre 2 y 3 pero no entre 5.`);
+    }
+  } else {
+    document.write(`${numero1} es divisible entre 2 pero no entre 3.`);
+  }
+} else {
+  document.write(`${numero1} no es divisible entre 2.`);
+}
--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,1 +1,8 @@
-const frase =prompt("Ingrese una frase: ")
+const frase = prompt("Ingrese una frase: ");
+
+document.write("El carácter en el índice 0 es '" + frase.charAt(0) + "' <br>");
+document.write("El carácter en el índice 1 es '" + frase.charAt(1) + "' <br>");
+document.write("El carácter en el índice 2 es '" + frase.charAt(2) + "' <br>");
+document.write("El carácter en el índice 3 es '" + frase.charAt(3) + "' <br>");
+document.write("El carácter en el índice 4 es '" + frase.charAt(4) + "' <br>");
+document.write("El carácter en el índice 999 es '" + frase.charAt(5) + "' ");
